0737dba3587a8e0582f60ec4c94afe039dda212c
jakejack13 <kerrj@bxscience.edu>
2019-03-27 14:55:20 -0400

    Added necessary files for GUI

--- GridTest.java
+++ GridTest.java
@@ -20,8 +20,18 @@ public class GridTest extends JFrame {
     JButton[][] arr = new JButton[10][10];
     for (int i = 0; i < 10; i++) {
       for (int j = 0; j < 10; j++) {
-        arr[i][j] = new JButton("",new ImageIcon("black.jpg"));
+        JButton b = new JButton(new ImageIcon("black.jpg"));
+        b.setPressedIcon(new ImageIcon("red.jpg"));
+        /*
+        arr[i][j].addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent arg0) {
+            label1.setText("Name has been submitted.");
+          }          
+        });
+        */
         //button.setBounds(0,0,100,100);
+        arr[i][j] = b;
         pane.add(arr[i][j]);
       }
     }
