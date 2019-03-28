51ada86c40b8438868443de739e17447fe8dac5d
jakejack13 <kerrj@bxscience.edu>
2019-03-28 14:10:12 -0400

    Set up grid UI

--- GridTest.java
+++ GridTest.java
@@ -4,11 +4,11 @@ import javax.swing.*;
 public class GridTest extends JFrame {
 
   public static void main(String[] args) {
-    int rows = 10;
-    int cols = 10;
+    int rows = 3;
+    int cols = 3;
     GridTest gt = new GridTest(rows, cols);
     gt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    gt.setSize(500,500);
+    gt.setSize(800,800);
     //gt.pack();
     gt.setVisible(true);
   }
@@ -16,24 +16,32 @@ public class GridTest extends JFrame {
   public GridTest(int rows, int cols) {
     super("Test");
     Container pane = getContentPane();
+    JPanel panel = new JPanel();
+    panel.setLayout(new GridLayout(10,10));
     pane.setLayout(new GridLayout(rows, cols));
     JButton[][] arr = new JButton[10][10];
     for (int i = 0; i < 10; i++) {
       for (int j = 0; j < 10; j++) {
         JButton b = new JButton(new ImageIcon("black.jpg"));
-        b.setPressedIcon(new ImageIcon("red.jpg"));
-        /*
-        arr[i][j].addActionListener(new ActionListener() {
-          @Override
-          public void actionPerformed(ActionEvent arg0) {
-            label1.setText("Name has been submitted.");
-          }          
-        });
-        */
-        //button.setBounds(0,0,100,100);
+        b.setSelectedIcon(new ImageIcon("redpic.jpg"));
         arr[i][j] = b;
-        pane.add(arr[i][j]);
+        panel.add(arr[i][j]);
       }
+    }
+    for (int n = 0; n < 4; n++) {
+      JPanel p = new JPanel();
+      p.setLayout(new GridLayout(10,10));
+      for (int i = 0; i < 100; i++)
+        p.add(new JLabel("O"));
+      pane.add(p);
+    }
+     pane.add(panel);
+     for (int n = 0; n < 4; n++) {
+      JPanel p = new JPanel();
+      p.setLayout(new GridLayout(10,10));
+      for (int i = 0; i < 100; i++)
+        p.add(new JLabel("O"));
+      pane.add(p);
     }
   }
 }
