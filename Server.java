57fc03b8042d090c13a28f8f72036510e4842dc6
BlueTitanium <36556764+BlueTitanium@users.noreply.github.com>
2019-03-31 15:03:13 -0400

    FIXED THE HUGE BUG!!!!!!!

--- Server.java
+++ Server.java
@@ -11,36 +11,29 @@ import java.util.*;
  */
 public class Server extends JFrame implements ActionListener, WindowListener{
   //Normal fields
-  private static int uniqID;
-  private ArrayList<ClientThread> al;
-  private int port;
-  private boolean keepGoing;
-  private String hostIP;
-  private Server server;
+  private static String hostIP;
+  private Helper server;
   //GUI fields
   private static final long serialVersionUID = 1L;
-  private JButton stopStartHost, startGame;
-  private JTextArea event;
-  private JTextField tPortNumber, tHostIP;
+  private static JButton stopStartHost, startGame;
+  private static JTextArea event;
+  private static JTextField tPortNumber, tHostIP;
   //server constructor
   public Server(int port){
     //init all vars
     super("Server");
-    this.port = port;
-    al = new ArrayList<ClientThread>();
     server = null;
-    try{
-      InetAddress ip = InetAddress.getLocalHost();
-      hostIP = ip.getHostAddress();
-    }catch(Exception e){
-      System.out.println(e);
-    }
-    
     JPanel north = new JPanel();
     north.add(new JLabel("Port number: "));
     tPortNumber = new JTextField("  " + port);
     north.add(tPortNumber);
     north.add(new JLabel("Host IPv4: "));
+    try{
+        InetAddress ip = InetAddress.getLocalHost();
+        hostIP = ip.getHostAddress();
+      }catch(Exception e){
+        System.out.println(e);
+      }
     tHostIP = new JTextField(" " + hostIP);
     tHostIP.setEditable(false);
     north.add(tHostIP);
@@ -67,76 +60,11 @@ public class Server extends JFrame implements ActionListener, WindowListener{
     //starts server on specified port number 
     new Server(1500);
   }
-  //start method
-  public void start(){
-    //create server socket and wait for connection reqs
-    //uses boolean to establish infinite while loop
-    keepGoing = true;
-    try{
-      ServerSocket sSocket = new ServerSocket(port);
-      while(keepGoing){
-        System.out.println("Server is accepting clients on this port: " + port + ".");
-        Socket socket = sSocket.accept();
-        if(!keepGoing)break;
-        ClientThread t = new ClientThread(socket);
-        al.add(t);
-        t.start();
-      }
-      try{
-        sSocket.close();
-        for(int i = 0; i < al.size(); i++){
-          ClientThread ct = al.get(i);
-          try{
-            ct.sIn.close();
-            ct.sOut.close();
-            ct.socket.close();
-          }catch(IOException ioe){
-            System.out.println(ioe);
-          }
-        }
-      }catch(Exception e){
-        System.out.println(e);
-      }
-    }catch(IOException e){
-      System.out.println(e);
-    }
-  }
-  protected void stop(){
-    //turn boolean false
-    //disable server
-    keepGoing = false;
-    hostIP = "localhost";
-    //looked around on the internet
-    //this is the best thing I found for exiting
-    //The server has to connect  to itself as a client
-    //If you read this and find a better method, please tell me!
-    try{
-      InetAddress ip = InetAddress.getLocalHost();
-      hostIP = ip.getHostAddress();
-    }catch(Exception e){System.out.println(e);}
-    try {
-      new Socket(hostIP, port);
-    }catch(Exception e){}
-  }
   private synchronized void display(){
     //This will attempt to display the minesweeper screens to
     //the clients as they appear
     
     //Implement after GUI of client is figured out
-  }
-  synchronized void rmclient(int id){
-    //takes id as arg
-    //when a client leaves, the client with the id has to be removed
-    //from the client thread arraylist
-    //this will scan the arraylist until we find id
-    for(int i = 0; i < al.size(); i++){
-      ClientThread client = al.get(i);
-      //finds id -> removes id and then exits method
-      if(client.id == id){
-        al.remove(i);
-        return;
-      }
-    }
   }
   //actionlistener method
   public void actionPerformed(ActionEvent e){
@@ -151,6 +79,7 @@ public class Server extends JFrame implements ActionListener, WindowListener{
       server = null;
       tPortNumber.setEditable(true);
       stopStartHost.setText("Start");
+      startGame.setEnabled(false);
       return;
     }
     //cannot reach this statement if server is running because it would go through the
@@ -163,37 +92,11 @@ public class Server extends JFrame implements ActionListener, WindowListener{
         event.append("> Invalid port number!\n");event.setCaretPosition(event.getText().length()-1);
         return;
       }
-      /*
-       * MAJOR BUG OVER HERE
-       * VVVVVVVVVVVVVVVVVVV
-       */
-      server = new Server(port);
+      server = new Helper(port);
       new ServerRunner().start();
       stopStartHost.setText("Stop");
       tPortNumber.setEditable(false);
       startGame.setEnabled(true);
-      /*
-       * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-       * This piece of code does not
-       * correctly start the program.
-       * It works in that it can start 
-       * the server, but it manages to 
-       * start up a new client?
-       * ________________________________
-       * 
-       * Potential fix would be to
-       * create a helper class and move 
-       * parts from this class to that 
-       * class. This series of comments
-       * will be deleted as soon as the
-       * bug gets fixed. However, the
-       * server does start up, so it is
-       * not particularly terrible in 
-       * terms of the scale of the issue.
-       * Will fix as soon as other parts 
-       * are implemented and I have time
-       * to bug fix.
-       */
       return;
     }
     if(o == startGame){
@@ -230,68 +133,145 @@ public class Server extends JFrame implements ActionListener, WindowListener{
       server = null;
     }
   } 
-  //inner class client thread
-  class ClientThread extends Thread{
-    Socket socket;
-    ObjectInputStream sIn;
-    ObjectOutputStream sOut;
-    Object input;
-    int id;
-    String username;
-    //constructor
-    ClientThread(Socket socket){
-      //init vars
-      //make sure each id is unique based on the uniqID
-      id = uniqID++;
-      this.socket = socket;
-      event.append("> Thread attempting to create IO streams\n");
-      event.setCaretPosition(event.getText().length()-1);
+  static class Helper{
+    private static int uniqID;
+    private ArrayList<ClientThread> al;
+    private int port;
+    private boolean keepGoing;
+
+    public Helper(int port){
+      this.port = port;
+      al = new ArrayList<ClientThread>();
+      
+    }
+    //start method
+    public void start(){
+      //create server socket and wait for connection reqs
+      //uses boolean to establish infinite while loop
+      keepGoing = true;
       try{
-        sOut = new ObjectOutputStream(socket.getOutputStream());
-        sIn = new ObjectInputStream(socket.getInputStream());
-        username = (String) sIn.readObject();
-        event.append("> " + username + " entered the fray!\n");
-        event.setCaretPosition(event.getText().length()-1);
-      }catch(IOException e){
-        event.append("> Exception with initializing IOStreams: " + e + "\n");
-        event.setCaretPosition(event.getText().length()-1);
-        return;
-      }
-      catch(ClassNotFoundException e){}
-    }
-    public void run(){
-      //this will run forever until logout
-      //this will handle all of the server inputs and displays for each Client Thread
-      boolean kG = true;
-      while(kG){
+        ServerSocket sSocket = new ServerSocket(port);
+        while(keepGoing){
+          System.out.println("Server is accepting clients on this port: " + port + ".");
+          Socket socket = sSocket.accept();
+          if(!keepGoing)break;
+          ClientThread t = new ClientThread(socket);
+          al.add(t);
+          t.start();
+        }
         try{
-          input = sIn.readObject();
+          sSocket.close();
+          for(int i = 0; i < al.size(); i++){
+            ClientThread ct = al.get(i);
+            try{
+              ct.sIn.close();
+              ct.sOut.close();
+              ct.socket.close();
+            }catch(IOException ioe){
+              System.out.println(ioe);
+            }
+          }
+        }catch(Exception e){
+          System.out.println(e);
         }
-        catch(IOException e){
-          //display error
-          break;
-        }
-        catch(ClassNotFoundException ee){break;}
-        //display based on input
-        
+      }catch(IOException e){
+        System.out.println(e);
       }
-      remove(id);
-      close();
     }
-    public void close(){
-      //closes connection
-      try{if(sOut != null)sOut.close();}
-      catch(Exception e){}
-      try{if(sIn != null)sIn.close();}
-      catch(Exception e){}
-      try{if(socket != null)socket.close();}
-      catch(Exception e){}
+    protected void stop(){
+      //turn boolean false
+      //disable server
+      keepGoing = false;
+      hostIP = "localhost";
+      //looked around on the internet
+      //this is the best thing I found for exiting
+      //The server has to connect  to itself as a client
+      //If you read this and find a better method, please tell me!
+      try{
+        InetAddress ip = InetAddress.getLocalHost();
+        hostIP = ip.getHostAddress();
+      }catch(Exception e){System.out.println(e);}
+      try {
+        new Socket(hostIP, port);
+      }catch(Exception e){}
     }
-    private boolean write(){
-      //send displays to output stream
-      //returns false to check if the client is not connected and removes it if it isn't
-      //returns true otherwise
-      return true;
+    synchronized void rmclient(int id){
+      //takes id as arg
+      //when a client leaves, the client with the id has to be removed
+      //from the client thread arraylist
+      //this will scan the arraylist until we find id
+      for(int i = 0; i < al.size(); i++){
+        ClientThread client = al.get(i);
+        //finds id -> removes id and then exits method
+        if(client.id == id){
+          al.remove(i);
+          return;
+        }
+      }
     }
-  } 
+    //inner class client thread
+    class ClientThread extends Thread{
+      Socket socket;
+      ObjectInputStream sIn;
+      ObjectOutputStream sOut;
+      Object input;
+      int id;
+      String username;
+      //constructor
+      ClientThread(Socket socket){
+        //init vars
+        //make sure each id is unique based on the uniqID
+        id = uniqID++;
+        this.socket = socket;
+        event.append("> Thread attempting to create IO streams\n");
+        event.setCaretPosition(event.getText().length()-1);
+        try{
+          sOut = new ObjectOutputStream(socket.getOutputStream());
+          sIn = new ObjectInputStream(socket.getInputStream());
+          username = (String) sIn.readObject();
+          event.append("> " + username + " entered the fray!\n");
+          event.setCaretPosition(event.getText().length()-1);
+        }catch(IOException e){
+          event.append("> Exception with initializing IOStreams: " + e + "\n");
+          event.setCaretPosition(event.getText().length()-1);
+          return;
+        }
+        catch(ClassNotFoundException e){}
+      }
+      public void run(){
+        //this will run forever until logout
+        //this will handle all of the server inputs and displays for each Client Thread
+        boolean kG = true;
+        while(kG){
+          try{
+            input = sIn.readObject();
+          }
+          catch(IOException e){
+            //display error
+            break;
+          }
+          catch(ClassNotFoundException ee){break;}
+          //display based on input
+          
+        }
+        rmclient(id);
+        close();
+      }
+      public void close(){
+        //closes connection
+        try{if(sOut != null)sOut.close();}
+        catch(Exception e){}
+        try{if(sIn != null)sIn.close();}
+        catch(Exception e){}
+        try{if(socket != null)socket.close();}
+        catch(Exception e){}
+      }
+      private boolean write(){
+        //send displays to output stream
+        //returns false to check if the client is not connected and removes it if it isn't
+        //returns true otherwise
+        return true;
+      }
+    } 
+  }
 }
